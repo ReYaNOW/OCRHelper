249036229e6dc2f8f1f4b5b571b4faa7399cc1f5
Sergei <reyangood@gmail.com>
2024-01-12 04:47:00 +0300

    make import and loads of EasyOCR model in different thread via threading module

--- ocrhelper/app.py
+++ ocrhelper/app.py
@@ -1,8 +1,11 @@
+import threading
+
 import numpy
 import pyperclip
 from PIL import Image
 from loguru import logger
 
+from components import languages
 from ocrhelper.components import config
 from ocrhelper.components.translation import translation
 from ocrhelper.components.utils import check_path
@@ -17,19 +20,37 @@ class App:
     def __init__(self):
         self.easyocr = None
         self.easyocr_model = None
+        self.ocr_is_loaded = False
         self.languages = None
         self.use_gpt_stream = False
 
         self.gui = Gui(self.snip_trigger, self.load_easyocr_with_toast)
+        self.load_ocr_threading(first_time=True)
 
-        self.gui.after(15, self.easyocr_first_time_load)
-        # run gui
         self.gui.mainloop()
 
-    def easyocr_first_time_load(self):
+    def load_ocr_threading(self, first_time=False):
         self.gui.load_ocr_toast.show_toast()
         self.gui.update()
+        self.ocr_is_loaded = False
 
+        if first_time:
+            target = self.easyocr_first_time_load
+        else:
+            target = self.load_easyocr_model
+        thread = threading.Thread(target=target)
+        thread.start()
+        self.check_thread(thread)
+
+    def check_thread(self, thread):
+        if thread.is_alive():
+            self.gui.after(100, lambda: self.check_thread(thread))
+        else:
+            self.gui.load_ocr_toast.hide_toast_immediately()
+            self.gui.loaded_ocr_toast.show_toast()
+            self.ocr_is_loaded = True
+
+    def easyocr_first_time_load(self):
         logger.info('Импорт модуля EasyOCR')
         import easyocr
 
@@ -41,16 +62,16 @@ class App:
 
         img = Image.open(check_path('additional files/load_easyocr.png'))
         self.easyocr_model.readtext(numpy.array(img))
-
-        self.gui.load_ocr_toast.hide_toast_immediately()
-        self.gui.loaded_ocr_toast.show_toast()
+        config.change_value('ocr_is_loaded', True)
 
     def load_easyocr_model(self):
+        config.change_value('ocr_is_loading', True)
         languages = self.languages
 
         logger.info(f'Загрузка модели EasyOCR c ' f'{", ".join(languages)}')
         self.easyocr_model = self.easyocr.Reader(languages)
         logger.success('Модель EasyOCR была успешно загружена')
+        config.change_value('ocr_is_loading', False)
 
     def load_easyocr_with_toast(self):
         """Load EasyOCR with some languages
@@ -58,12 +79,9 @@ class App:
         new_languages = config.get_value('recognition_languages')
         if self.languages != new_languages:
             self.languages = new_languages
-
-            self.gui.load_ocr_toast.show_toast()
-            self.gui.update()
-            self.load_easyocr_model()
-            self.gui.load_ocr_toast.hide_toast_immediately()
-            self.gui.loaded_ocr_toast.show_toast()
+            self.load_ocr_threading()
+        else:
+            self.gui.already_loaded_toast.show_toast()
 
     def snip_trigger(self, image: Image.Image, coordinates: tuple):
         """Trigger when a screenshot is taken. Performs OCR on the image,
@@ -73,7 +91,9 @@ class App:
             image: The captured screenshot image.
             coordinates: The coordinates of the captured screenshot.
         """
-        self.gui.debug_window.add_message('Скриншот был получен', 'green')
+        self.gui.debug_window.add_message(
+            languages.get_string('received_screenshot'), 'green'
+        )
         self.load_easyocr_with_toast()
 
         text = TextRecognition(
@@ -82,7 +102,7 @@ class App:
 
         translator = config.get_value('translator')
         self.gui.debug_window.add_message(
-            f'Перевод при помощи —\n{translator}\n',
+            f'{languages.get_string("translation_with")} —\n{translator}\n',
             color='white',
         )
 
@@ -90,7 +110,6 @@ class App:
         translated_text = translation(
             text=text,
             from_lang=self.languages,
-            to_lang='russian',
             translator=translator,
         )
         logger.success('Текст успешно переведен')
@@ -116,5 +135,7 @@ class App:
             image,
             text_related,
         )
-        self.gui.debug_window.add_message('Перевод прошел успешно!', 'green')
+        self.gui.debug_window.add_message(
+            languages.get_string('transl_succeed'), 'green'
+        )
         self.use_gpt_stream = False
